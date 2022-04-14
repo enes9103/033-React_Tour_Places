c5cf226698266cd1b3ae0661ff4afd13c023a23b
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2022-04-14 11:31:48 +0300

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,22 +5,22 @@ function Navbar() {
     <div>
         <ul>
             <li>
-                <a href="aboutus">ABOUT US</a>
+                <a href="#aboutus">ABOUT US</a>
             </li>
             <li>
-                <a href="foryou">FOR YOU</a>
+                <a href="#foryou">FOR YOU</a>
             </li>
             <li>
-                <a href="services">SERVICES</a>
+                <a href="#services">SERVICES</a>
             </li>
             <li>
-                <a href="blog">BLOG</a>
+                <a href="#blog">BLOG</a>
             </li>
             <li>
-                <a href="vlog">VLOG</a>
+                <a href="#vlog">VLOG</a>
             </li>
             <li>
-                <a href="contact">CONTACT</a>
+                <a href="#contact">CONTACT</a>
             </li>
         </ul>
     </div>
